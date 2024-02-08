b7707fbd84621fe0c7fe71fec22d3cae71aae4f8
shital_avhad <120552343+shitalavhad@users.noreply.github.com>
2024-02-07 23:06:48 -0800

    Update Simple.java

--- GitHubPractice/src/demo/Simple.java
+++ GitHubPractice/src/demo/Simple.java
@@ -5,6 +5,12 @@ public class Simple {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hiiii");
+		System.out.println("hiiii");
+
+		System.out.println("hiiii");
+
+		System.out.println("hiiii");
+
 
 	}
 
